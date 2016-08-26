d8b724f5a0322f8c4f8d7fd1e3af64ed5d38e841
vincentLiuxiang <hitliuyaguang@foxmail.com>
2016-08-26 14:13:45 +0800

    connect example

--- example/connect-example.js
+++ example/connect-example.js
@@ -21,25 +21,32 @@ app.use(function (req,res,next) {
   })
 });
 
-
 var server = http.Server(app);
 
+var ws = websocket(server)
+  .on('data',function (obj) {
+    console.log(obj.type,obj.buffer.length);
+  })
+  .on('pong',function (text) {
+    console.log('pong ...',text);
+  })
+
 // websocket
-server.on('upgrade',function (req,socket,head) {
-  var ws = websocket(socket)
-    .shakeHand(req)
-    .receiveFrame()
-    .on('data',function(obj){
-      console.log(obj.type,obj.buffer.length);
-      switch(obj.type) {
-        case 'string':
-          ws.send(obj.buffer.toString());
-        break;
-        case 'binary':
-          ws.send(obj.buffer,'binary');
-        break;
-      }
-    });
-})
+// server.on('upgrade',function (req,socket,head) {
+//   var ws = websocket(socket)
+//     .shakeHand(req)
+//     .receiveFrame()
+//     .on('data',function(obj){
+//       console.log(obj.type,obj.buffer.length);
+//       switch(obj.type) {
+//         case 'string':
+//           ws.send(obj.buffer.toString());
+//         break;
+//         case 'binary':
+//           ws.send(obj.buffer,'binary');
+//         break;
+//       }
+//     });
+// })
 
 server.listen(3000);
