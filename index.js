177064e2259030634774dbffaf1f277a2e27581c
vincentLiuxiang <hitliuyaguang@foxmail.com>
2016-12-23 00:53:40 +0800

    move lib/* ./

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-module.exports = require('./lib/websocket');
+var EventEmitter = require('events').EventEmitter;
+var websocket = require('./websocket');
+var http = require('http');
+
+
+function Server (server,config) {
+  this.server = server;
+  this.config = config || {};
+}
+
+Server.prototype = Object.create(EventEmitter.prototype);
+Server.prototype.constructor = Server;
+
+Server.prototype.onUpgrade = function () {
+  var self = this;
+  self.server.on('upgrade', function (req,socket,head) {
+    var ws = websocket(socket,self.config)
+    ws.auth = self.auth;
+    ws.on('connect',function (s) {
+      self.emit('connect',s)
+    })
+    .shakeHand(req,socket)
+    .receiveFrame();
+  });
+  return this;
+}
+
+module.exports = function (server,config) {
+  return new Server(server,config).onUpgrade();
+}
